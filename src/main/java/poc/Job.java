9d7699db06e4ce06f17d2f18b97089fcce0714e6
mjwa <mikolaj.wozniak@gft.com>
2020-11-13 12:33:54 +0100

    Job

--- src/main/java/poc/Job.java
+++ src/main/java/poc/Job.java
@@ -3,15 +3,14 @@ package poc;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.streaming.api.TimeCharacteristic;
-import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import poc.fun.*;
+import poc.fun.CounterProcessFunction;
+import poc.fun.MySink;
+import poc.fun.ProcessJoin;
 import poc.model.CountModel;
-import poc.model.CountedDataModel;
 import poc.model.DataModel;
-import poc.model.MultiplyDataModel;
 import poc.sources.CountSource;
 import poc.sources.DataSource;
 
@@ -22,9 +21,10 @@ public class Job {
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        env.setParallelism(5);
 
-        DataStreamSource<DataModel> dataSource = env.addSource(new DataSource());
-        DataStreamSource<CountModel> countSource = env.addSource(new CountSource());
+        SingleOutputStreamOperator<DataModel> dataSource = env.addSource(new DataSource()).name("Data Source");
+        SingleOutputStreamOperator<CountModel> countSource = env.addSource(new CountSource()).name("Count Source");
 
         KeyedStream<DataModel, Integer> dataByKey =
                 dataSource.keyBy((KeySelector<DataModel, Integer>) dataModel -> dataModel.id);
@@ -34,12 +34,16 @@ public class Job {
         dataByKey
                 .connect(countByKey)
                 .process(new ProcessJoin())
+                .name("ProcessJoin")
                 .keyBy(c -> c.data.id)
                 .process(new CounterProcessFunction())
+                .name("CounterProcessFunction")
                 .keyBy(c -> c.data.id)
                 .process(new MultiplyFun())
+                .name("MultiplyFun")
                 .keyBy(c -> c.id)
                 .addSink(new MySink())
+                .name("Sink ...")
                 .setParallelism(1); //TODO Consider split sink to storage and warehouse store procedure. Then setParallelism 1 only for store procedure.
 
         env.execute("Job");
